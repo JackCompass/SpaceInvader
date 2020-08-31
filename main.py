b190d74839b02c257ca33ea8373a2689179dad2a
Jack <kumaranuj1303@gmail.com>
2020-08-31 23:33:48 +0530

    enemey plane and shooting added

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 window = pygame.display.set_mode((500, 600))
 pygame.display.set_caption("Space Invader")
 space_craft = pygame.image.load('spaceship.png')
+enemy_craft = pygame.image.load('enemy1.png')
 space_background = pygame.image.load('spacebg1.jpg')
 clock = pygame.time.Clock()
 
 
 class SpaceCraft(object):
-	def __init__(self, x, y, height, width, velocity):
+	def __init__(self, x, y, velocity):
 		self.x = x
 		self.y = y
-		self.height = height
-		self.width = width
 		self.velocity = velocity
 
 	def draw(self, window):
 		window.blit(space_craft, (self.x, self.y))
+
+
+class EnemySpaceCraft(object):
+	def __init__(self, x, y, velocity):
+		self.x = x
+		self.y = y
+		self.velocity = velocity
+
+	def draw(self, window):
+		window.blit(enemy_craft, (self.x, self.y))
+
 
 
 class Bullets(object):
@@ -36,31 +47,83 @@ class Bullets(object):
 # for drawing all elements in the game.
 def draw_elements():
 	window.blit(space_background, (0, 0))
+	plane.draw(window)
 	for bullet in bullets:
 		bullet.draw(window)
-	plane.draw(window)
+	for enemy in enemies:
+		enemy.draw(window)
+	for bullet in enemy_bullets:
+		bullet.draw(window)
+	
 	pygame.display.update()
 
 # object
-plane = SpaceCraft(0, 520, 2, 2, 10)
+plane = SpaceCraft(0, 520, 10)
+enemies = list()
 bullets = list()
 bullet_recoil = 0
+fps = 0
+enemy_generator = 0
+enemy_bullets = list()
 
 # Gameloop
 while True:
+
+	# FPS manager
 	clock.tick(30)
+
+	# time delay between two enemies.
+	if enemy_generator > 0:
+		enemy_generator -= 1
+
+	# time delay between two bullets player{POV}
 	if bullet_recoil > 0:
 		bullet_recoil -= 1
+
+	if fps < 30:
+		fps += 1
+		bullet_stopper = False
+	else:
+		fps = 0
+		bullet_stopper = True
+
+	# mange the exit point of the game
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 
+	# manage the enemy bullet velocity and distance bullet travel.
+	for bullet in enemy_bullets:
+		if bullet.y < 580:
+			bullet.y += bullet.velocity
+		else:
+			enemy_bullets.pop(enemy_bullets.index(bullet))
+
+	# manage the player bullet velocity and distance bullet travel.
 	for bullet in bullets:
 		if bullet.y > 20:
 			bullet.y -= bullet.velocity
 		else:
 			bullets.pop(bullets.index(bullet))
-		
+
+	# manage the tajectory of the enemy plane
+	for enemy in enemies:
+		if (enemy.y < 500) and (0 < enemy.x < 470):
+			enemy.y += enemy.velocity
+			enemy.x += (enemy.velocity * random.choice([-1, 1]))
+			if bullet_stopper:
+				enemy_bullets.append(Bullets(enemy.x + 50, enemy.y + 70, 3, (255, 255, 255), 8))
+		else:
+			enemies.pop(enemies.index(enemy))
+
+	# control no of enemies at a time.
+	if len(enemies) < 5:
+		if enemy_generator == 0:
+			enemies.append(EnemySpaceCraft(random.randint(0, 480), 0, 3))
+			enemy_generator = 5
+
+
+	# key management.
 	keys = pygame.key.get_pressed()
 	if keys[pygame.K_LEFT] and plane.x >= -50:
 		plane.x -= plane.velocity
